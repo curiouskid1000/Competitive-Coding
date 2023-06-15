a83308cdb93fba6a92462129a40002f466543622
curiouskid1000 <86244752+curiouskid1000@users.noreply.github.com>
2023-06-15 23:25:01 +0530

    Add files via upload

---
+++ leetcode/1221.splitthestring.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int l = 0;
+        int r = 0;
+        for(int i =0; i<s.length(); i++){
+            if(s.charAt(i) == 'L'){
+                l++;
+            }
+            else{
+                l--;
+            }
+            if(l == 0){
+                r++;
+            }
+        }
+        return r;
+    }
+} 
